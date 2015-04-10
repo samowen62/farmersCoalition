e26785019fb1a89cc691e7219186362b9d97c011
samowen62 <sam@hardindd.com>
2015-04-10 16:06:52 -0500

    tyring fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session, 
+  #  if: Proc.new { |c| c.request.format =~ %r{'application/json'} }
+  #  end
   protect_from_forgery with: :null_session, 
-    if: Proc.new { |c| c.request.format =~ %r{'application/json'} }
+    only: Proc.new { |c| c.request.format.json? }
     end
+
   #skip_before_action :verify_authenticity_token
 
 
