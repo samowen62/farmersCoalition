d89ee7298e5554c2721cbfeb33f2088a4278c116
samowen62 <sam@hardindd.com>
2015-04-10 19:43:54 -0500

    fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :null_session
-  #protect_from_forgery with: :null_session, 
-  #  if: Proc.new { |c| c.request.format =~ %r{'application/json'} }
-  #  end
-  protect_from_forgery with: :null_session, 
-    only: Proc.new { |c| c.request.format.json? }
-    end
 
-  #skip_before_action :verify_authenticity_token
-
+  skip_before_filter :verify_authenticity_token, if: :json_request?
 
   protected
   def authenticate_user
