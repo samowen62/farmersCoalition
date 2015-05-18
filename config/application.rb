523634be8db699b117b91aab21359c462c6880fa
samowen62 <sam@hardindd.com>
2015-05-18 12:47:20 -0500

    changing x-frame options

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module FarmersCoalition
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_dispatch.default_headers['X-Frame-Options'] = "SAMEORIGIN, GOFORIT"
+    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOWALL"
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
