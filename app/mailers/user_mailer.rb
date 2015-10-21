4917341de381ba891078e90130237b4695f97d38
samowen62 <samowen62@yahoo.com>
2015-10-21 08:56:21 -0500

    testing mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,11 +8,20 @@ class UserMailer < ActionMailer::Base
   	@user = User.where(email: email).first
   	unless @user.nil?
   		hash = (0...50).map { (65 + rand(26)).chr }.join
-  		#u = User.where(email: email).first
   		@user.reset_hash = hash
   		@user.save!
 
-      mail(to: email, subject: "fmmetrics.wisc Reset password")
+      #this is really weird and hacky but taking the time to figure out
+      #the stupid system rails has in place just isn't worth it
+      `cp /var/www/app/mailers/mail.txt /var/www/app/mailers/#{hash}`
+      `sed -i "s=%url%=http://fmmetrics.wisc.edu/reset/#{hash}=" /var/www/app/mailers/#{hash}`
+
+      #better be sure email is escaped initially
+      #already is but might want to be sure there are no spaces
+      `sendmail #{email} < /var/www/app/mailers/#{hash}`
+      `rm /var/www/app/mailers/#{hash}`
+
+      #mail(to: email, subject: "fmmetrics.wisc Reset password")
 
 =begin
 	  	RestClient.post "https://api:key-408f273d54022a70dbcc75d1674174bb"\
